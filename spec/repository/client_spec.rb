a890a19702ba168da800d18980e7a56794d1e7a4
Justin Coyne <jcoyne@justincoyne.com>
2019-11-15 07:31:53 -0600

    Initial gem

---
+++ spec/repository/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Repository::Client do
+  it "has a version number" do
+    expect(Repository::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
