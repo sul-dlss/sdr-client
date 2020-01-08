8e1af1598a4ba2f60156ea9f90ce100ecc242ca5
Naomi Dushay <ndushay@stanford.edu>
2020-01-07 23:22:40 -0800

    rubocop: add frozen_string_literal comment

--- lib/repository/client.rb
+++ lib/repository/client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "repository/client/version"
 
 module Repository
--- spec/repository/client_spec.rb
+++ spec/repository/client_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Repository::Client do
   it "has a version number" do
     expect(Repository::Client::VERSION).not_to be nil
