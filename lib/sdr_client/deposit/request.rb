362a8bf58f56afed7f26251b92855fd3f15a39a1
Justin Coyne <jcoyne@justincoyne.com>
2020-01-09 15:09:41 -0800

    Support basic metadata

--- lib/sdr_client/deposit/request.rb
+++ lib/sdr_client/deposit/request.rb
@@ -4,33 +4,47 @@ module SdrClient
   module Deposit
     # This represents the metadata that we send to the server for doing a deposit
     class Request
-      CONTEXT = 'http://cocina.sul.stanford.edu/contexts/cocina-base.jsonld'
-
       # @param [String] label the required object label
       # @param [String] type (http://cocina.sul.stanford.edu/models/object.jsonld) the required object type.
       # @param [Array<SdrClient::Deposit::Files::DirectUploadResponse>] uploads the uploaded files to attach.
-      def initialize(label:,
+      def initialize(label: nil,
+                     apo:,
+                     collection:,
+                     source_id:,
+                     catkey: nil,
                      type: 'http://cocina.sul.stanford.edu/models/object.jsonld',
                      uploads: [])
         @label = label
         @type = type
+        @source_id = source_id
+        @collection = collection
+        @catkey = catkey
+        @apo = apo
         @uploads = uploads
       end
 
       def as_json
-        {
-          "@context": CONTEXT,
-          "@type": type,
-          label: label,
+        json = {
+          type: type,
+          administrative: {
+            hasAdminPolicy: apo
+          },
+          identification: {
+            sourceId: source_id
+          },
           structural: {
+            isMemberOf: collection,
             hasMember: file_sets_as_json
           }
         }
+        json[:label] = label if label
+        json[:identification][:catkey] = catkey if catkey
+        json
       end
 
       private
 
-      attr_reader :label, :uploads, :type
+      attr_reader :label, :uploads, :source_id, :catkey, :apo, :collection, :type
 
       # In this case there is a 1-1 mapping between Files and FileSets,
       # but this doesn't always have to be the case.  We could change this in the
@@ -38,8 +52,7 @@ module SdrClient
       def file_sets_as_json
         uploads.map do |upload|
           {
-            "@context": CONTEXT,
-            "@type": 'http://cocina.sul.stanford.edu/models/fileset.jsonld',
+            "type": 'http://cocina.sul.stanford.edu/models/fileset.jsonld',
             label: upload.filename,
             structural: {
               hasMember: [upload.signed_id]
