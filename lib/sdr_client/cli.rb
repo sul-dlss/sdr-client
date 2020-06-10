891f675242ae7014385d317121327b2ad7e763d1
Justin Coyne <jcoyne@justincoyne.com>
2020-06-10 10:53:32 -0500

    Display help message when no command is given

--- lib/sdr_client/cli.rb
+++ lib/sdr_client/cli.rb
@@ -3,6 +3,37 @@
 module SdrClient
   # The command line interface
   module CLI
+    HELP = <<~HELP
+      DESCRIPTION:
+        The SDR Command Line Interface is a tool to interact with the Stanford Digital Repository.
+
+      SYNOPSIS:
+        sdr [options] <command>
+
+        To see help text for each command you can run:
+
+        sdr [options] <command> help
+
+      OPTIONS:
+        --service-url (string)
+        Override the command's default URL with the given URL.
+
+        -h, --help
+        Displays this screen
+
+
+      COMMANDS:
+        deposit
+          Accession an object into the SDR
+
+        register
+          Create a draft object in SDR and retrieve a Druid identifier.
+
+        login
+          Will prompt for email & password and exchange it for an login token, which it saves in ~/.sdr/token
+
+    HELP
+
     def self.start(command, options)
       case command
       when 'deposit'
@@ -18,6 +49,11 @@ module SdrClient
     rescue SdrClient::Credentials::NoCredentialsError
       puts 'Log in first'
       exit(1)
+    end
+
+    def self.help
+      puts HELP
+      exit
     end
   end
 end
