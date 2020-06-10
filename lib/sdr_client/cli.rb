9f7532909b77524fe973d91542344d178703025e
Justin Coyne <jcoyne@justincoyne.com>
2020-06-10 11:39:39 -0500

    Display a message about how to fix missing arguments

--- lib/sdr_client/cli.rb
+++ lib/sdr_client/cli.rb
@@ -36,10 +36,9 @@ module SdrClient
 
     def self.start(command, options)
       case command
-      when 'deposit'
-        SdrClient::Deposit.run(accession: true, **options)
-      when 'register'
-        SdrClient::Deposit.run(accession: false, **options)
+      when 'deposit', 'register'
+        display_errors(validate_deposit_options(options))
+        SdrClient::Deposit.run(accession: command == 'deposit', **options)
       when 'login'
         status = SdrClient::Login.run(options)
         puts status.value if status.failure?
@@ -49,6 +48,19 @@ module SdrClient
     rescue SdrClient::Credentials::NoCredentialsError
       puts 'Log in first'
       exit(1)
+    end
+
+    def self.display_errors(errors)
+      return if errors.empty?
+
+      raise errors.map { |k, v| "#{k} #{v}" }.join("\n")
+    end
+
+    def self.validate_deposit_options(options)
+      {}.tap do |errors|
+        errors['admin-policy'] = 'is a required argument' unless options[:apo]
+        errors['source-id'] = 'is a required argument' unless options[:source_id]
+      end
     end
 
     def self.help
