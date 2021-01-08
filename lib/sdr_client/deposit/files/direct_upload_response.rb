0a0147ed1ea7d5f2096ef161593c0fc2f0e75536
Johnathan Martin <john.martin@stanford.edu>
2021-01-08 08:59:16 -0800

    add missing param to DirectUploadResponse struct definition

--- lib/sdr_client/deposit/files/direct_upload_response.rb
+++ lib/sdr_client/deposit/files/direct_upload_response.rb
@@ -5,7 +5,7 @@ module SdrClient
     module Files
       DirectUploadResponse = Struct.new(:id, :key, :checksum, :byte_size, :content_type,
                                         :filename, :metadata, :created_at, :direct_upload,
-                                        :signed_id, keyword_init: true)
+                                        :signed_id, :service_name, keyword_init: true)
     end
   end
 end
