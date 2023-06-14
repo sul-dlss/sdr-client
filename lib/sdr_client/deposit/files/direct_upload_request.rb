d51d4ce9b70eaa4bda52d470dc7319eb82c99f52
Peter Mangiafico <peter@mangiafico.org>
2023-06-14 14:03:02 -0700

    refactor and rename cleaning method

--- lib/sdr_client/deposit/files/direct_upload_request.rb
+++ lib/sdr_client/deposit/files/direct_upload_request.rb
@@ -10,14 +10,14 @@ module SdrClient
           checksum = Digest::MD5.file(path).base64digest
           new(checksum: checksum,
               byte_size: ::File.size(path),
-              content_type: clean_content_type(content_type),
+              content_type: clean_and_translate_content_type(content_type),
               filename: file_name)
         end
 
         def as_json
           {
             blob: { filename: filename, byte_size: byte_size, checksum: checksum,
-                    content_type: self.class.clean_content_type(content_type) }
+                    content_type: self.class.clean_and_translate_content_type(content_type) }
           }
         end
 
@@ -27,18 +27,19 @@ module SdrClient
 
         # Invalid JSON files with a content type of application/json will trigger 400 errors in sdr-api
         # since they are parsed and rejected (not clear why and what part of the stack is doing this).
-        # The work around is to change the content_type for any JSON files to something different and
-        # specific to avoid the parsing, and thaen have this changed back to application/json after .
-        # upload is complete. There is a similar change in sdr-api to change the content_type back.
+        # The work around is to change the content_type for any JSON files to a custom stand-in and
+        # specific to avoid the parsing, and then have this translated back to application/json after .
+        # upload is complete. There is a corresponding change in sdr-api to translate the content_type back
+        # before the Cocina is saved.
         # See https://github.com/sul-dlss/happy-heron/issues/3075 for the original bug report
         # See https://github.com/sul-dlss/sdr-api/pull/585 for the change in sdr-api
-        def self.clean_content_type(content_type)
+        def self.clean_and_translate_content_type(content_type)
           return 'application/octet-stream' if content_type.blank?
 
-          return 'application/x-stanford-json' if content_type == 'application/json'
-
           # ActiveStorage is expecting "application/x-stata-dta" not "application/x-stata-dta;version=14"
-          content_type.split(';')&.first
+          content_type = content_type.split(';')&.first
+
+          content_type == 'application/json' ? 'application/x-stanford-json' : content_type
         end
       end
     end
